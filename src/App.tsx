8d9d965058fd9b45d3a4dc2c79a7305754710727
heath smith <heathsmith73@gmail.com>
2022-06-12 20:43:03 -0700

    update name

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import { WebCam } from './types/web_cam_type';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { Home } from './views/home';
+import { About } from './views/about';
+import { LiveWebCams } from './views/live_views';
+import { fetchMultiWebcam } from './api_calls/windy_api';
+import { florence101Id, BayshoreNewportId, pacificCityId, narragansettBeachId } from './consts/web_cam_ids';
+
+import './app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [images, setImages] = useState<Array<WebCam>>([]);
+
+	const getWebcams = async () => {
+		const boom = await fetchMultiWebcam([florence101Id, BayshoreNewportId, pacificCityId, narragansettBeachId]);
+		setImages(boom);
+	};
+
+	useEffect(() => {
+		if (!images.length) {
+			getWebcams();
+		}
+	}, [images]);
+
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/live'>Live images</Link>
+						</li>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<header className='app-header'>For Tim</header>
+
+				<Routes>
+					<Route path='/' element={<Home images={images} />} />
+					<Route path='/live' element={<LiveWebCams images={images} />} />
+					<Route path='/about' element={<About />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
